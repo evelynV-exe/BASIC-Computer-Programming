f7ebe44e072aa2d4a8793bf2cca9eef0158ae0c5
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-11-19 16:44:45 +0700

    Update Lab1.1.cpp

--- Lab1.1.cpp
+++ Lab1.1.cpp
@@ -1,1 +1,18 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
 
+int main() {
+    int N, i;
+    
+    // รับค่า N (จำนวนรอบ)
+    if (scanf("%d", &N) != 1) {
+        return 1; // Handle input failure
+    }
+    
+    // TODO: Implement the for loop to print the message N times
+    for (int i = 0; i < N; i++) {
+        printf("Hello Loop!\n");
+    }
+
+    return 0;
+}
