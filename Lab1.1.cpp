ed4afc6676148ca56a0723bb3db6c37489a8bebf
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-11-19 17:04:03 +0700

    fix for error handling
    Added error handling for zero and negative input values.

--- Lab1.1.cpp
+++ Lab1.1.cpp
@@ -7,12 +7,18 @@ int main() {
     // รับค่า N (จำนวนรอบ)
     if (scanf("%d", &N) != 1) {
         return 1; // Handle input failure
-    }
+    } //end if
+
+    //Error handling for zero and negative numbers
+    if (N <= 0) {
+        printf("Error");
+        return 1;
+    } // end if
     
     // TODO: Implement the for loop to print the message N times
     for (int i = 0; i < N; i++) {
         printf("Hello Loop!\n");
-    }
+    } // end if
 
     return 0;
-}
+} // end function
