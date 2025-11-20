5a7d8582a3a9ff6885d9c3e874b051be30474714
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-11-20 19:43:50 +0700

    Add files via upload

---
+++ basic1.4.cpp
@@ -0,0 +1,20 @@
+// Pre-code (โค้ดบังคับ)
+#include <stdio.h>
+
+int main() {
+    int N, sum = 0, i;
+    
+    // รับค่า N
+    if (scanf("%d", &N) != 1) {
+        return 1; // Handle input failure
+    }
+    
+    // TODO: Implement the for loop to calculate the sum from 1 to N
+
+    for (int i = 1; i <= N; i++) {
+        sum += i;
+    }
+
+    printf("%d\n", sum);
+    return 0;
+}
