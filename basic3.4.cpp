54e38bc3c698becf25475ea32d7ddbc73727ab31
Evelynnn <93260139+evelynV-exe@users.noreply.github.com>
2025-12-02 10:43:39 +0700

    Update basic3.4.cpp

--- basic3.4.cpp
+++ basic3.4.cpp
@@ -8,21 +8,23 @@ struct Student {
 
 int main() {
     int N, i;
-    int passCount = 0;
-    int failCount = 0;
+    int passCount = 0, failCount = 0;
 
+    // Read number of students
     if (scanf("%d", &N) != 1) {
         return 1;
     }
 
     struct Student students[N];
-    
+
+    // Read student info (id, score, name)
     for (i = 0; i < N; i++) {
         if (scanf("%d %f %s", &students[i].studentId, &students[i].score, students[i].name) != 3) {
             return 1;
         }
     }
 
+    // Count pass/fail students
     for (i = 0; i < N; i++) {
         if (students[i].score >= 60.0) {
             passCount++;
@@ -31,8 +33,9 @@ int main() {
         }
     }
 
-    printf("Pass Count: %d\n", passCount);
-    printf("Fail Count: %d\n", failCount);
+    // output
+    printf("Pass count: %d\n", passCount);
+    printf("Fail count: %d\n", failCount);
 
     return 0;
 }
